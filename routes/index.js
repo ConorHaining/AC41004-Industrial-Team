3db7cedf532b649d6cadf9fcee368ff032d64e18
AndreiDombrovschi <AndreiDombrovschi@computing.dundee.ac.uk>
2018-09-11 10:40:41 +0100

    Added a route for a basic wrdl3d map just for testing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express'});
+});
+
+router.get('/wrld3d', function(req, res, next){
+  res.render('wrld3d');
 });
 
 module.exports = router;
