e3191d8b5e2adf56d6728a44277e1e1a84eb5640
astryx <robertasdere@gmail.com>
2018-09-13 11:40:58 +0100

    Initial commit for the noise generator branch

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/noiseGenerator', function(req, res, next) {
+  res.render('noiseGenerator');
+});
+
 module.exports = router;
