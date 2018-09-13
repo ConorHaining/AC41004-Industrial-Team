bed6ff48200a32a5bbee9abf3c03cf8b818f6a84
Dombrovschi <dombrovschi.andrei1996@gmail.com>
2018-09-13 11:53:58 +0100

    Playing around with different charts

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,8 @@ router.get('/wrld3d', function(req, res, next){
   res.render('wrld3d');
 });
 
+router.get('/testing', function(req, res, next){
+  res.render('testing');
+});
+
 module.exports = router;
