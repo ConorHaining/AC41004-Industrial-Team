c2ea9f654c57107ac822420be8cb21ced0b9640d
Conor Haining <conor.haining@gmail.com>
2018-09-15 20:50:54 +0100

    Ajax request for popup HTML

--- public/scripts/timetable.js
+++ public/scripts/timetable.js
@@ -30,49 +30,29 @@ const locations = [
 ]
 let timetable;
 
-$.get({
-    url: 'timetable/GroundFloorLabs/today',
-    dataType: 'json',
-    success: (data) => {
-        timetable = data;
-    } 
-});
-
-function timetableTableBuilder(timetable) {
-    let tableHTML;
-
-    tableHTML += '<thead><td>Module</td><td>Start</td><td>End</td><td>Staff</td></thead>';
-    tableHTML += '<tbody>';
-    
-    timetable.forEach((record) => {
-        tableHTML += '<tr>';
-        tableHTML += `<td>${record.title}</td>`
-        tableHTML += `<td>${record.start}</td>`
-        tableHTML += `<td>${record.end}</td>`
-        tableHTML += `<td>${record.staff.toString()}</td>`
-        tableHTML += '</tr>';
-    });
-    
-    tableHTML += '</tbody>';
-    return tableHTML;
-}
-
 function placeMarkers(event) {
     const buildingName = event.indoorMap.getIndoorMapId();
 
     locations.forEach((location) => {
 
-        var marker = L.marker(location.LatLong, {        
-            title: location.Title,
-            indoorMapId: location.IndoorMapId,
-            indoorMapFloorId: location.IndoorMapFloor
-          }).addTo(map)
-            .bindPopup(`<h1>${location.Title}</h1>
-                        <h3>Today's Timetable</h3>
-                        <table>${timetableTableBuilder(timetable)}</table>`,
-                {
-                    maxWidth: 600,
-                });
+        $.get({
+            url: 'timetable/GroundFloorLabs/today',
+            dataType: 'html',
+            success: (data) => {
+                
+                L.marker(location.LatLong, {        
+                    title: location.Title,
+                    indoorMapId: location.IndoorMapId,
+                    indoorMapFloorId: location.IndoorMapFloor
+                  }).addTo(map)
+                    .bindPopup(data,
+                        {
+                            maxWidth: 600,
+                        });
+
+            } 
+        });
+
     });
 
 }
