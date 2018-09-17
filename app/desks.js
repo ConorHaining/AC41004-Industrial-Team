711e30a95ec7a07a6c50a4e3eb0a5a0d35e06795
Conor Haining <conor.haining@gmail.com>
2018-09-17 15:43:47 +0100

    Randomly generate table occupancy

--- app/desks.js
+++ app/desks.js
@@ -1,12 +1,15 @@
 
 module.exports = {
     getCurrentDeskUsage: () => {
-        const n = Date.now();
+        
+        const desks = ['0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009', '0010', '0011', '0012', '0013', '0014', '0015', '0016', '0017', '0018', '0019', '0020', '0021', '0022', '0023', '0024', '0025', '0026', '0027', '0028', '0029', '0030', '0031', '0032', '0033', '0034', '0035', '0036', '0037', '0038', '0039'];
 
-        if(n % 2 === 1) {
-            return {'0001': true, '0002': true, '0003': false, '0004': true, '0006': true, '0007': false, '0008': false, '0009': true, '0010': true};
-        } else {
-            return {'0001': true, '0002': false, '0003': false, '0004': false, '0006': true, '0007': true, '0008': true, '0009': true, '0010': true};
-        }
+        let occupancy = {};
+
+        desks.forEach((desk) => {
+            let r = Math.random();
+            if (r < 0.5) { occupancy[desk] = true; } else { occupancy[desk] = false; }
+        });
+        return occupancy;
     }
 }
