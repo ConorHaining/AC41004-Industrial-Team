ee55a5b8a54bbc9e0a1cf86ad3172d96c8a99f5c
Holly Helton <hollyrachelhelton@gmail.com>
2018-09-20 15:10:35 +0100

    Programatically generated the list of pc names to ping

--- app/ping.js
+++ app/ping.js
@@ -1,18 +1,24 @@
-var elasticsearch = require('elasticsearch');
-var ping = require('ping');
+const elasticsearch = require('elasticsearch');
+const ping = require('ping');
+
+let pclist = [];
+for(let labNo = 0; labNo < 3; labNo++) {
+    for(let deskNo = 1; deskNo < 8; deskNo++) {
+        for(let pcNo = 1; pcNo < 5; pcNo++) {
+            pclist.push("labpc"+labNo+deskNo+pcNo)
+        }
+    }
+}
 
 client = new elasticsearch.Client({
     hosts: ['https://elastic:VToECC5TLZoafrR83FfRSC3A@0196297975c7432f95b7a548aa467123.eu-west-1.aws.found.io:9243/']
 });
 
-let pclist = ['labpc071', 'labpc072', 'labpc073', 'labpc074',
-    'labpc021', 'labpc022', 'labpc023', 'labpc024'];
-
 setInterval(() => {
     pclist.forEach(function (pcname) {
         host = pcname + ".computing.dundee.ac.uk";
         ping.sys.probe(host, function (isAlive) {
-            var body = {
+            let body = {
                 "timestamp": Date.now(),
                 "pc_name": pcname,
                 "available": isAlive
