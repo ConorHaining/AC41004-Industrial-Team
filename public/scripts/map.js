9a8ddefcb5459086ccb64ae912f343356882bab7
Conor Haining <conor.haining@gmail.com>
2018-09-23 14:45:47 +0100

    Use the latest QMB manifest

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -2,7 +2,7 @@ var map = L.Wrld.map("map", "03013be32ed00b5339b756582b8d184a", {
     center: [56.458629, -2.982733],
     zoom: 18,
     indoorsEnabled: true,
-    coverageTreeManifest: "https://webgl-cdn1.wrld3d.com/chunk/indoor_maps/api_requests/EIM-e97931c8-3b30-4525-bd5a-743aeaff7cb1_2018_09_17_08_39_34/webgl_manifest.bin.gz"
+    coverageTreeManifest: "https://webgl-cdn1.wrld3d.com/chunk/indoor_maps/api_requests/EIM-7e76aeae-2f60-471d-baac-cf0d75dc91cf_2018_09_21_10_34_42/webgl_manifest.bin.gz"
   });
 
 var indoorControl = new WrldIndoorControl("widget-container", map);
