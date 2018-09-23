4d1117ed8055dd1b4e016a7b7083f1117416797f
Conor Haining <conor.haining@gmail.com>
2018-09-23 16:32:23 +0100

    Updated tests

--- test/desks.js
+++ test/desks.js
@@ -4,21 +4,23 @@ var desks = require('../app/desks.js');
 describe('Desk Availability', () => {
     it('should return an object of desks being used', () => {
         
-        let deskAvailability = desks.getCurrentDeskUsage();
+        desks.getCurrentDeskUsage((deskAvailability) => {
+            deskAvailability.should.be.an('object');
+        });
 
-        deskAvailability.should.be.an('object');
 
     });
 
     it('should contain either true/false as values', () => {
 
-        let deskAvailability = desks.getCurrentDeskUsage();
-
-        let values = Object.values(deskAvailability);
-
-        values.forEach(element => {
-            element.should.be.a('boolean');
+        desks.getCurrentDeskUsage((deskAvailability) => {
+            let values = Object.values(deskAvailability);
+    
+            values.forEach(element => {
+                element.should.be.a('boolean');
+            });
         });
+
 
     });
 });
