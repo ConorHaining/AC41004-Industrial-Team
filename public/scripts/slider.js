6f0b3c80c6e5626999035c3352cf3e00572841a6
astryx <robertasdere@gmail.com>
2018-09-24 15:08:55 +0100

    Added new highlight function for tables on specific hours.

--- public/scripts/slider.js
+++ public/scripts/slider.js
@@ -1,16 +1,19 @@
 var slider = document.getElementById("hourRange");
 let tempDate = new Date();
 slider.value = tempDate.getHours();
+displaySliderTime(tempDate);
 //output.innerHTML = slider.value;
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
     //output.innerHTML = this.value;
-    displaySliderTime();
+    let curDate = new Date();
+    displaySliderTime(curDate);
+    deskHighlightingHour(curDate.getHours());
 }
 
-function displaySliderTime() {
-    let curDate = new Date();
+function displaySliderTime(curDate) {
+    
     var popup = document.getElementById("myPopup");
     if(curDate.getHours() == slider.value)
         popup.innerHTML = "Time: Current";
@@ -20,4 +23,22 @@ function displaySliderTime() {
     {
         popup.classList.toggle("show");
     }*/
+}
+
+function updateSliderTime()
+{
+    let curDate = new Date();
+    if ( popup.innerHTML == "Time: Current" )
+    {
+        if ( slider.value != curDate.getHours() )
+        {
+            slider.value = curDate.getHours();
+            displaySliderTime(curDate);
+        }
+    }
+    else
+    {
+        if ( slider.value == curDate.getHours() )
+            displaySliderTime(curDate);
+    }
 }
