3622dfb7166b09e839f5f61f3432c3fa8603a10b
astryx <robertasdere@gmail.com>
2018-09-24 11:38:51 +0100

    Merged wrld3d/testing branch to master. Added noise gradient.

--- routes/index.js
+++ routes/index.js
@@ -9,21 +9,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express'});
 });
 
-router.get('/wrld3d', function(req, res, next){
-  res.render('wrld3d');
-});
-
-router.get('/noiseGenerator', function(req, res, next) {
-  res.render('noiseGenerator');
-});
-
-router.get('/graphTest', function(req, res, next) {
-  res.render('graphTest');
-});
-
 router.get('/timetable/:room', (req, res) => {
   res.send(timetable.getTimetableByRoom(req.params['room']));
 });
+
 router.get('/timetable/:room/today', (req, res) => {
   timetable.getTimetableByRoomToday(req.params['room'], (html) => {
     res.send(html);
