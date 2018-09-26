4de0a28648baa4257136e787d2d6b3a6840dfb63
Dombrovschi <dombrovschi.andrei1996@gmail.com>
2018-09-26 09:52:15 +0100

    made new changes to the graph

--- public/scripts/graphDisplay.js
+++ public/scripts/graphDisplay.js
@@ -1,12 +1,36 @@
 var e = document.getElementById("graphButton");
 var c = document.getElementById("myChart");
+var isElementClicked = false;
+var moveChart = false;
 
-e.onmouseover = function () {
-    c.style.visibility = "visible";
+function animate() {
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if(moveChart){
+        pos++;
+        c.style.top = pos + 'px';
+        c.style.left = pos +'px';
+        } else {
+            c.style.top = pos
+            c.style.left = pos;
+        }
+    }
 }
 
-e.onmouseout = function () {
-     c.style.visibility = "hidden";
+e.onclick = function () {
+    moveChart = !moveChart
+    if(!isElementClicked){
+    //animate();
+    c.style.visibility = "visible";
+    //console.log(c);
+    isElementClicked = true;
+    } else {
+        //animate();
+        c.style.visibility = "hidden";
+        isElementClicked = false;
+    }
+   // console.log(moveChart);
 }
 
 
